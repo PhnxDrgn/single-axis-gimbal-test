3ef6ed79f179f385b9ff3a79c9cd0e69bc965dd5
Andy Hoang <andyhoang2010@gmail.com>
2024-11-12 00:03:06 -0500

    added serial

--- app/src/app.c
+++ app/src/app.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include "app.h"
 #include "mpu6050.h"
+#include "stdio.h"
+#include "string.h"
 
 MPU6050_data_t mpuData;
 volatile bool mpuDataUpdated = false;
@@ -10,8 +12,14 @@ void APP_main()
     MPU6050_init(&mpuData);
     while (1)
     {
+        if (mpuDataUpdated)
+        {
+            char buffer[256];
+            memset(buffer, '\0', sizeof(buffer));
 
+            snprintf(buffer, sizeof(buffer), "gyroX: %7.02f\tgyroY: %7.02f\tgyroZ: %7.02f\n\r", mpuData.gyro.x, mpuData.gyro.y, mpuData.gyro.z);
 
+            serialPrint(buffer, strnlen(buffer, sizeof(buffer)));
         }
     }
 }
